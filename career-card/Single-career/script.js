15163f11cc211909de061fcbb50dc98b03b37b14
TareqMonower123 <tareq@lemonhive.com>
2025-06-30 12:05:40 +0600

    updated the application for for the career apply

--- career-card/Single-career/script.js
+++ career-card/Single-career/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
+  // mobile stickey trigger bar
   $("#sticky-trigger").click(function () {
     $(".wak-single-career-content-bottom-sticky-content").slideToggle(300);
+  });
+
+  // file upload view
+  $(".wak-single-career-apply-form-file-input").each(function () {
+    var defaultText = $(this).find(".wak-single-career-apply-form-file-input-icon p").text();
+    $(this).data("default-text", defaultText);
+  });
+
+  $('.wak-single-career-apply-form-file-input input[type="file"]').on("change", function (e) {
+    var fileName = e.target.files[0]?.name;
+    var container = $(this).closest(".wak-single-career-apply-form-file-input");
+    var displayElement = container.find(".wak-single-career-apply-form-file-input-icon p");
+
+    displayElement.text(fileName || container.data("default-text"));
   });
 });
