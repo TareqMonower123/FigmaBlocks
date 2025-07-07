13d3b1f5c73d3b085918c7b708bccb2df700a92c
test <test>
2025-07-07 10:40:49 +0600

    update script.js and style.scss

--- accordion/script.js
+++ accordion/script.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-  // active class for first accord
-  const firstItem = $(".wak-accordion-container-item").first();
-  firstItem.addClass("active");
-  firstItem.find(".wak-accordion-container-item-content-text").show();
-
   // functionality for new accord
   $(".wak-accordion-container-item-content-title, .wak-accordion-container-item-icons").click(
     function () {
@@ -13,9 +8,6 @@ $(document).ready(function () {
         item.removeClass("active");
         item.find(".wak-accordion-container-item-content-text").slideUp(300);
       } else {
-        $(".wak-accordion-container-item").removeClass("active");
-        $(".wak-accordion-container-item-content-text").slideUp(300);
-
         item.addClass("active");
         item.find(".wak-accordion-container-item-content-text").slideDown(300);
       }
