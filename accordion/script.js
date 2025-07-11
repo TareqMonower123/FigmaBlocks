b3d94a5dfad3e96f29ceeeabc483cb4bad2dba94
test <test>
2025-07-11 11:50:20 +0600

    accordion blocks updated

--- accordion/script.js
+++ accordion/script.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
   // functionality for new accord
-  $(".wak-accordion-container-item-content-title, .wak-accordion-container-item-icons").click(
+  $(".wak-accordion-container-item-icons-container").click(
     function () {
       const item = $(this).closest(".wak-accordion-container-item");
 
       if (item.hasClass("active")) {
         item.removeClass("active");
-        item.find(".wak-accordion-container-item-content-text").slideUp(300);
+        item.find(".wak-accordion-container-item-content").slideUp(300);
+        item.find(".wak-accordion-container-item-icons-icon-add").show();
+        item.find(".wak-accordion-container-item-icons-icon-remove").hide();
       } else {
         item.addClass("active");
-        item.find(".wak-accordion-container-item-content-text").slideDown(300);
+        item.find(".wak-accordion-container-item-content").slideDown(300);
+        item.find(".wak-accordion-container-item-icons-icon-add").hide();
+        item.find(".wak-accordion-container-item-icons-icon-remove").show();
       }
     }
   );
